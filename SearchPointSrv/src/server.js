875e9c999b74ba568556a8b1d01dbc01ccc1cd00
Joao Costa <joao.xlab@gmail.com>
2020-04-27 20:39:30 +0200

    reverting changes

--- SearchPointSrv/src/server.js
+++ SearchPointSrv/src/server.js
@@ -175,13 +175,14 @@ module.exports = exports = function (opts) {
     //serve static directories on the UI path
     app.use('/', express.static(path.join(__dirname, '../ui')));
 
-    var express = require('express');
-    var app = express();
+    //var express = require('express');
+    //var app = express();
     
     //setting middleware
-    app.use(express.static(_dirname + 'public')); //serving resources from public folder
     
-    var server = app.listen(5000);
+    //app.use(express.static(_dirname + 'public')); //serving resources from public folder
+    
+   // var server = app.listen(5000);
 
     initRestApi();
 
